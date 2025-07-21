df0ae5611dc1b20aaa51f95703f900dedce1db72
StJonas <jonasstoegerer@yahoo.at>
2025-07-21 12:40:45 +0200

    updated gratitude entry model for seeding

--- app/models/gratitude_entry.rb
+++ app/models/gratitude_entry.rb
@@ -1,6 +1,6 @@
 class GratitudeEntry < ApplicationRecord
   belongs_to :user
-  belongs_to :gratitude_prompt
+  belongs_to :gratitude_prompt, optional: true
 
   has_many :gratitude_entry_tags
   has_many :tags, through: :gratitude_entry_tags
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_21_074551) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_21_103918) do
   create_table "emotion_log_entries", force: :cascade do |t|
     t.integer "emotion_log_id", null: false
     t.integer "emotion_id", null: false
